04980d06b26bc3e8c390a2cd1b567a7e0d9c7ce4
Phinehas Enakireru <phinehasenakireru@gmail.com>
2022-12-16 10:43:35 +0100

    fixing gitignore

--- srcjs/Publisher.js
+++
@@ -1,8 +0,0 @@
-export class Publisher {
-    constructor(publisherName) {
-        this.publisherName = publisherName;
-    }
-    getPublisherName() {
-        return this.publisherName;
-    }
-}
